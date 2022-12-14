aff30771ce2c5453c24155ee2f5cd004f26f827c
Yulia Miller <miller82.98@mail.ru>
2022-12-14 00:25:47 -0800

    Добавьте файлы проекта.

---
+++ 4.2. cycle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            for(int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Iteration{0}", i);
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("This is your super color!");
+                        break;
+                }
+            }
+        }
+    }
+}
